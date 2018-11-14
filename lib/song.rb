7b3f1a52e42844c4746fef816a15da4826af7c41
Katie Ashlin <akka726@outlook.com>
2018-11-14 14:24:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,11 +32,22 @@ class Song
       if genre_count[genre]
         genre_count[genre] += 1
       else 
-        genre_count[genre]
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
   
   def self.artist_count
     artist_count = {}
+    @@artists each do |artist|
+      if artist_coun[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
 end
